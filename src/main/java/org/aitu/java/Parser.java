7753505c7dcb1467d147e1ebc28e72ed20d04c12
Muratbekuli Meyirzhan <smilexah@inbox.ru>
2023-03-30 14:13:53 +0600

    final.

--- src/main/java/org/aitu/java/Parser.java
+++ src/main/java/org/aitu/java/Parser.java
@@ -7,8 +7,8 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
 
 public class Parser {
     private static Document getPage() throws IOException {
@@ -17,14 +17,14 @@ public class Parser {
         return page;
     }
 
-    private static Pattern pattern = Pattern.compile("\\d{2}\\.\\d{2}");
-    private static String getDateFromString(String stringDate) throws Exception {
-        Matcher matcher = pattern.matcher(stringDate);
-        if (matcher.find()) {
-            return matcher.group();
-        }
-        throw new Exception("Can't extract date from string");
-    }
+//    private static Pattern pattern = Pattern.compile("\\d{2}\\.\\d{2}");
+//    private static String getDateFromString(String stringDate) throws Exception {
+//        Matcher matcher = pattern.matcher(stringDate);
+//        if (matcher.find()) {
+//            return matcher.group();
+//        }
+//        throw new Exception("Can't extract date from string");
+//    }
 
     private static int printPartValues(Elements values, int index) {
         int iterationCount = 4;
@@ -32,24 +32,16 @@ public class Parser {
             Element valueLn = values.get(3);
             boolean isMorning = valueLn.text().contains("Утро");
             if (isMorning) iterationCount = 3;
-
-            for (int i = 0; i < iterationCount; i++) {
-                Element valueLine = values.get(index + i);
-                for (Element td : valueLine.select("td")) {
-                    System.out.print(td.text() + "    ");
-                }
-                System.out.println();
-            }
-            return iterationCount;
-        } else {
-            for (int i = 0; i < 4; i++) {
-                Element valueLine = values.get(index);
-                for (Element td : valueLine.select("td")) {
-                    System.out.print(td.text() + "    ");
-                }
-                System.out.println();
-            }
         }
+
+        for (int i = 0; i < iterationCount; i++) {
+            Element valueLine = values.get(index + i);
+            for (Element td : valueLine.select("td")) {
+                System.out.print(td.text() + "    ");
+            }
+            System.out.println();
+        }
+
         return iterationCount;
     }
     public static void main(String[] args) throws Exception {
@@ -60,9 +52,10 @@ public class Parser {
         int index = 0;
 
         for (Element name : names) {
-            String dateString = name.select("th[id=dt]").text();
-            String date = getDateFromString(dateString);
-            System.out.println(date + "    Явления    Температура    Давл.    Влажность    Ветер");
+//            String dateString = name.select("th[id=dt]").text();
+            final String date = name.select("th[id=dt]").text().split("\\w+", 1)[0];
+//            String date = getDateFromString(dateString);
+            System.out.println(date + "     Явления     Температура     Давл.     Влажность     Ветер");
             int iterationCount  = printPartValues(values, index);
             index += iterationCount;
         }
